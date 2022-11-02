d39dc38db566008e85bdf16fa954e952aee3efc6
Kelsideris <kelsey.sideris@gmail.com>
2022-11-02 23:21:37 +0000

    Find local java and respons.

--- js/findlocal.js
+++ js/findlocal.js
@@ -1,3 +1,32 @@
+
+// const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday", "Sunday"];
+
+// let today = weekday[d.getDay()];
+// console.log(today);
+
+
+var answer= document.getElementById('answer');
+const d = new Date();
+var day = d.getDay();
+let hrs = d.getHours();
+
+
+
+answer.addEventListener('click', () => {
+    if(day <  5 && hrs < 16 && hrs >= 8 ){
+        answer.innerHTML = 'We are open. Take a break from work and come get some eggs!';
+    } else if(day == 5 && day == 6 && hrs < 22 && hrs >= 8 ) {
+        answer.innerHTML = 'We are open. Fresh eggs at the ready until 10pm!';
+    } else if(day == 7 && hrs < 22 && hrs >= 10) {
+        answer.innerHTML = 'We are open. Come get some sunday brunch!';
+    } else {
+        answer.innerHTML = 'Sorry, we are closed at the moment. We are open 7 days a week and will back at our stations cooking up a storm soon. Check times above.'; 
+        console.log(hrs);
+    }
+}
+)
+
+//Newsletter
 document.addEventListener("DOMContentLoaded", function() {
 
     // Newsletter sign up button - checks if input box contains an @ //
